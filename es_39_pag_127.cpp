d941f35e352880da2b31be23734e723f920a8c90
matteodecaprio <matteodc2011@libero.it>
2016-11-11 21:03:48 +0100

    Add files via upload

---
+++ es_39_pag_127.cpp
@@ -0,0 +1,27 @@
+//rette perpendicolari
+#include<iostream>
+using namespace std;
+
+bool perpendicolare(int x,int y);
+
+int main(){
+int m1,m2;
+cout<<"Quanto vale il primo coefficiente angolare?"<<endl;
+cin>>m1;
+cout<<"Quanto vale il secondo coefficiente angolare?"<<endl;
+cin>>m2;
+perpendicolare(m1,m2);
+return 0;	
+}
+
+bool perpendicolare(int x,int y){
+	bool vero;
+	if(x=-1/y){
+		vero=true;
+		cout<<"Le rette sono perpndicolari";
+	}else{
+		vero=false;
+		cout<<"le rette non sono perpendicolari";
+	}
+	return vero;
+}
